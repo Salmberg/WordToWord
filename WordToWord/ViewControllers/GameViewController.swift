e331b2b2406f964f20b07cb9c3a098d4257fbaf0
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-03-22 15:24:56 +0100

    Now it can check if the answer is right and give one pint

--- WordToWord/ViewControllers/GameViewController.swift
+++ WordToWord/ViewControllers/GameViewController.swift
@@ -7,35 +7,46 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,UITextFieldDelegate {
     
-    
-    
+    var rounds: Int? = 0
+    var points: Int? = 0
     var countdownTimer: Timer!
-    var totalTime = 3 // Total time for the countdown timer
-    var timeLeft = 3 // Current time left for the countdown timer
+    var totalTime = 5 // Total time for the countdown timer
+    var timeLeft = 5 // Current time left for the countdown timer
     
     @IBOutlet weak var countdownLabel: UILabel!
     
     @IBOutlet weak var wordToWriteLabel: UILabel!
     
-    let wordDataManager = WordDataManager()
+    @IBOutlet weak var userInputTextField: UITextField!
     
+    @IBOutlet weak var roundsLabel: UILabel!
+    
+    @IBOutlet weak var pointsLabel: UILabel!
+    
+    let wordDataManager = WordDataManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userInputTextField.becomeFirstResponder()
         
-        // Get a randomly selected word from WordDataManager
-        if let shuffledWord = wordDataManager.getRandomWord() {
-            // Set the text of wordLabel to the shuffled word
-            wordToWriteLabel.text = shuffledWord
-        }
+        userInputTextField.delegate = self
+        
+       
+        getNewWord()
             
-            // Initialize the countdown timer
-            countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-            
+           
         }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        checkAnswer()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        checkAnswer()
+    }
         
         // Function to update the countdown timer
         @objc func updateTimer() {
@@ -50,7 +61,48 @@ class GameViewController: UIViewController {
         func endTimer() {
             countdownTimer.invalidate()
             countdownLabel.text = "Time's up!"
-            
+            userInputTextField.text = ""
+            checkAnswer()
+            getNewWord()
+           
+        }
+        func getNewWord() {
+            if let shuffledWord = wordDataManager.getRandomWord() {
+                // Set the text of wordToWriteLabel to the shuffled word
+                wordToWriteLabel.text = shuffledWord
+                
+                // Reset the timer
+                timeLeft = totalTime
+                countdownLabel.text = "Time left: \(timeLeft)"
+                countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            }
+        }
+    
+    func checkAnswer() {
+        let wordToWrite = wordToWriteLabel.text
+        let userInput = userInputTextField.text
+        print("wordToWrite: \(wordToWrite)")
+        print("userInput: \(userInput)")
+        if wordToWrite == userInput {
+            print("Rätt!")
+            // The user input matches the displayed word
+            // Add a point to the points variable
+            if let currentPoints = points {
+                points = currentPoints + 1
+            } else {
+                points = 1
+            }
+            pointsLabel.text = "Points: \(points ?? 0)"
+        } else {
+            // The user input does not match the displayed word
+            // Do something to indicate that they got it wrong
         }
     }
+
+
+
+
+    }
+    
+
 
