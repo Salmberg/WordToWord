6892fe7e5ec437f8900be2c3d019c0a12f27e955
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-03-22 11:39:51 +0100

    Now i it can shuffle and display the shuffled word in the game

--- WordToWord/Model/WordData.swift
+++ WordToWord/Model/WordData.swift
@@ -14,7 +14,15 @@ struct Word{
 
 class WordDataManager {
 
-    static var listOfWords = [Word(word: "Bil"), Word(word: "Katt"), Word(word: "Äpple"), Word(word: "Havet"), Word(word: "Trädgård"), Word(word: "Sjö"), Word(word: "Solen"), Word(word: "Vattenfall"), Word(word: "Vind"), Word(word: "Fjäril"), Word(word: "Vatten"), Word(word: "Flod"), Word(word: "Berg"), Word(word: "Moln"), Word(word: "Regn"), Word(word: "Snö"), Word(word: "Skog"), Word(word: "Mån"), Word(word: "Stjärna"), Word(word: "Jord"), Word(word: "Ö"), Word(word: "Kust"), Word(word: "Vindkraft"), Word(word: "Fisk"), Word(word: "Natur"), Word(word: "Skärgård"), Word(word: "Fågel"), Word(word: "Guldstjärna"), Word(word: "Sommar"), Word(word: "Höst")]
+       var listOfWords = [Word(word: "Bil"), Word(word: "Katt"), Word(word: "Äpple"), Word(word: "Havet"), Word(word: "Trädgård"), Word(word: "Sjö"), Word(word: "Solen"), Word(word: "Vattenfall"), Word(word: "Vind"), Word(word: "Fjäril"), Word(word: "Vatten"), Word(word: "Flod"), Word(word: "Berg"), Word(word: "Moln"), Word(word: "Regn"), Word(word: "Snö"), Word(word: "Skog"), Word(word: "Mån"), Word(word: "Stjärna"), Word(word: "Jord"), Word(word: "Ö"), Word(word: "Kust"), Word(word: "Vindkraft"), Word(word: "Fisk"), Word(word: "Natur"), Word(word: "Skärgård"), Word(word: "Fågel"), Word(word: "Guldstjärna"), Word(word: "Sommar"), Word(word: "Höst")]
+    
+    
+    func getRandomWord() -> String? {
+    let shuffledWords = listOfWords.shuffled()
+    return shuffledWords.first?.word
 
+}
+
+   
 }
 
--- WordToWord/ViewControllers/GameViewController.swift
+++ WordToWord/ViewControllers/GameViewController.swift
@@ -10,22 +10,31 @@ import UIKit
 class GameViewController: UIViewController {
     
     
-
-    var countdownTimer: Timer!
-        var totalTime = 3 // Total time for the countdown timer
-        var timeLeft = 3 // Current time left for the countdown timer
-        
-        @IBOutlet weak var countdownLabel: UILabel!
     
-        @IBOutlet weak var wordToWriteLabel: UILabel!
+    var countdownTimer: Timer!
+    var totalTime = 3 // Total time for the countdown timer
+    var timeLeft = 3 // Current time left for the countdown timer
+    
+    @IBOutlet weak var countdownLabel: UILabel!
+    
+    @IBOutlet weak var wordToWriteLabel: UILabel!
+    
+    let wordDataManager = WordDataManager()
     
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         
+        
+        // Get a randomly selected word from WordDataManager
+        if let shuffledWord = wordDataManager.getRandomWord() {
+            // Set the text of wordLabel to the shuffled word
+            wordToWriteLabel.text = shuffledWord
+        }
             
             // Initialize the countdown timer
             countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            
         }
         
         // Function to update the countdown timer
@@ -41,5 +50,7 @@ class GameViewController: UIViewController {
         func endTimer() {
             countdownTimer.invalidate()
             countdownLabel.text = "Time's up!"
+            
         }
-}
+    }
+
